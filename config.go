d063a343195797aebf4b4a3d8a6e9e0e01bd432b
Dmitry <me@dfilimonov.com>
2019-07-21 13:14:39 -0700

    sets a more conservative default mem-threshold

--- config.go
+++ config.go
@@ -14,7 +14,7 @@ func (c *config) populateFromFlags() {
 	flag.StringVar(&c.logLevel, "log-level", "info", "log level (debug / info / error)")
 	flag.StringVar(&c.statsdAddr, "statsd-addr", ":8125", "statsd address to bind to")
 	flag.StringVar(&c.httpAddr, "http-addr", ":10000", "http endpoint address to bind to")
-	flag.StringVar(&c.memThreshold, "mem-threshold", "80%", "maximum amount of memory heatmap is allowed to take")
+	flag.StringVar(&c.memThreshold, "mem-threshold", "70%", "maximum amount of memory heatmap is allowed to take")
 	flag.BoolVar(&c.createDummyData, "create-dummy-data", false, "flag that enables dummy data generation")
 	flag.Parse()
 }
